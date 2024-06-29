73cc25e9c8263a91e94a533a9228a87b46bbff46
Misstes <you@example.com>
2024-06-28 21:53:42 -0500

    Add input tag component with delete functionality and prevent duplicate tags

---
+++ src/components/InputTag.vue
@@ -0,0 +1,94 @@
+<script>
+export default {
+    props: ["onTagsChange"],
+    data() {
+        return {
+            currentValue: "",
+            tags: [],
+        };
+    },
+
+    methods: {
+        handleKeyDown(event) {
+            if (event.key === 'Backspace' && this.currentValue === '') {
+                this.tags.pop();
+                this.onTagsChange(this.tags);
+            }
+        },
+        handleSubmit() {
+            if (this.currentValue !== '') {
+                const exist = this.tags.some(item => item === this.currentValue);
+                if (!exist) {
+                    this.tags.push(this.currentValue);
+                    this.currentValue = '';
+                    this.onTagsChange(this.tags);
+                }
+            }
+        },
+        deleteTag(tag) {
+            this.tags = this.tags.filter(t => t !== tag);
+            this.onTagsChange(this.tags);
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="inputTag">
+        <div class="tags">
+            <div class="tag" v-for="(tag, index) in tags" :key="index">
+                {{ tag }} <button @click="deleteTag(tag)">X</button>
+            </div>
+        </div>
+        <form @submit.prevent="handleSubmit">
+            <input class="input" type="text" v-model="currentValue" @keydown="handleKeyDown" />
+        </form>
+    </div>
+
+</template>
+
+<style scoped>
+.inputTag {
+    display: inline-flex;
+    border: solid 1px #000;
+    border-radius: 3px;
+    height: 43px;
+}
+
+.tags {
+    display: flex;
+    gap: 3px;
+    padding: 5px;
+}
+
+.tags .tag{
+    display: flex;
+    padding: 5px;
+    border: solid 1px #ccc;
+    gap: 5px;
+    align-items: center;
+    border-radius: 3px;
+}
+
+.inputTag form{
+   display: inline-flex;
+}
+
+.inputTag .input{
+    border: none;
+    outline: none;
+    padding: 0 5px;
+}
+
+.inputTag button{
+    background-color: transparent;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+}
+
+.inputTag button:hover{
+    background-color: #eee;
+}
+
+</style>
